aaeb041b3cd77eb1ba8da6d3544f1442d5f8196f
Sensess <SensessYentzer@outlook.com>
2022-07-16 17:23:17 -0400

    File Structure + JWT, Need to Implement Auth Middlewar to Populate Me Query

---
+++ Server/schemas/typeDefs.js
@@ -0,0 +1,36 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    friendCount: Int
+    friends: [User]
+  }
+  type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+  }
+
+`;
+
+module.exports = typeDefs;
