486279cecbe4fc685b1f8648df194e47e4d90f9f
britbolt <blattisaw@gmail.com>
2022-07-28 16:36:54 -0400

    added chakra to results

--- client/src/pages/QuizPage.js
+++ client/src/pages/QuizPage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Box, Container, VStack, FormControl, Button, FormLabel, Image, Heading, Text, useBoolean, FormErrorMessage } from '@chakra-ui/react';
+import { Box, Container, Divider, VStack, FormControl, Button, FormLabel, Image, Heading, Text, useBoolean, FormErrorMessage } from '@chakra-ui/react';
 import { Select, CreatableSelect } from "chakra-react-select";
 import { useForm, useController } from "react-hook-form";
 // import { Button, Container, Row, Form, Card, CardGroup } from "react-bootstrap";
@@ -114,61 +114,64 @@ const QuizPage = () => {
 
   const handleUserSelection = async () => {
     try {
-      const response = await fetchListTitles(sourceValues, genreValues, typeValues);
-      
+      const response = await fetchListTitles(
+        sourceValues,
+        genreValues,
+        typeValues
+      );
 
       if (!response.ok) {
         throw new Error("something went wrong!");
       }
 
       const { titles } = await response.json();
-      
 
       const titleData = titles.map((title) => ({
-        titleId: title.id
+        titleId: title.id,
       }));
 
-      setSearchedTitles(titleData); 
-      console.log(titleData[0].titleId);
-   
+      setSearchedTitles(titleData);
 
-      const detailsResponse = await fetchTitleDetails((titleData[0].titleId));
-      if (!detailsResponse.ok) {
-        throw new Error("something went wrong!");
+      let detailsResponse = [];
+
+      for (let index = 0; index < titleData.length; index++) {
+        const detailsRes = await fetchTitleDetails(titleData[index].titleId);
+
+        const json = await detailsRes.json();
+        detailsResponse.push(json);
+
+        if (!detailsRes.ok) {
+          throw new Error("something went wrong!");
+        }
       }
 
-      const { details } = await detailsResponse.json();
-
-      const detailsData = details.map((detail) => ({
+      const detailsData = detailsResponse.map((detail) => ({
         titleId: detail.id,
         title: detail.title,
         plotOverview: detail.plot_overview,
-        type: detail.type,
+        type: detail.type.charAt(0).toUpperCase() + detail.type.slice(1),
         poster: detail.poster,
-        runtimeMinutes: detail.runtime_minutes,
-        genreNames: detail.genre_names || ['No genres to display'],
+        runtimeMinutes:
+          detail.runtime_minutes + " mins" || "No runtime to display",
+        genreNames: detail.genre_names.join(", ") || "No genres to display",
         userRating: detail.user_rating,
         criticsRating: detail.critic_score,
-        originalLanguage: detail.original_language,
-        networkNames: detail.network_names || ['No networks to display'],
-        trailer: detail.trailer,
-        sources: detail.sources || ['No sources to display'],
-
+        networkNames: detail.network_names || "No networks to display",
+        trailer: detail.trailer || "No trailers to display",
       }));
 
       setDisplayedTitles(detailsData);
-
     } catch (err) {
       console.error(err);
     }
   };
 
-  
-
   // create function to handle saving a title to our database
   const handleSaveTitle = async (titleId) => {
     // find the title in `searchedTitles` state by the matching id
-    const titleToSave = searchedTitles.find((title) => title.titleId === titleId);
+    const titleToSave = displayedTitles.find(
+      (title) => title.titleId === titleId
+    );
 
     // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
@@ -180,7 +183,7 @@ const QuizPage = () => {
     try {
       await saveTitle({
         variables: { titleData: { ...titleToSave } },
-      });     
+      });
 
       // if title successfully saves to user's account, save title id to state
       setSavedTitleIds([...savedTitleIds, titleToSave.titleId]);
@@ -189,30 +192,29 @@ const QuizPage = () => {
     }
   };
   
+ 
+  const handleChangeSource = (event) => {
+    if (event.target === "select-multiple") {
+      setSourceValues(
+        [...event.target.selectedOptions].map((option) => option.value)
+      );
+    }
+  };
 
-  // const handleChangeSource = (event) => {
-  //     setSourceValues(
-  //     [...event.target.defaultValues].map((option) => option.value)
-  //   );
-  // };
-
-  // const handleChangeType = (event) => {
-  //   // event.stopImmediatePropagation();
-  //   // event.preventDefault();
-
-  //   setTypeValues(
-  //     [...event.target.typeValues].map((option) => option.value)
-  //   );
-  // };
-  // const handleChangeGenre = (event) => {
-  //   // event.stopImmediatePropagation();
-  //   // event.preventDefault();
-
-  //   setGenreValues(
-  //     [...event.target.genreValues].map((genres) => genres.value)
-  //   );
-  
-
+  const handleChangeType = (event) => {
+    if (event.target === "select-multiple") {
+      setTypeValues(
+        [...event.target.selectedOptions].map((option) => option.value)
+      );
+    }
+  };
+  const handleChangeGenre = (event) => {
+    if (event.target === "select-multiple") {
+      setGenreValues(
+        [...event.target.selectedOptions].map((option) => option.value)
+      );
+    }
+  };
  
 
 const onSubmit = data => {
@@ -221,19 +223,12 @@ handleUserSelection(data)};
 
   const defaultValues = { types: [], genres: [], sources: [] };
 
-  const { control, handleSubmit } = useForm({ defaultValues });
+  const { control, handleSubmit } = useForm({ defaultValues, mode: 'onSubmit' });
 
   const [isLoading, setLoading] = useBoolean(false);
 
  
-    // const submit = async (data) => {
-    //   setLoading.on();
-    //   setTimeout(() => {
-    //     setLoading.off();
-    //     alert(JSON.stringify(data, null, 2));
-    //   }, 3000);
-    // };
-
+ 
   if (error) {
     return <div>Error: {error.message}</div>;
   } else if (!isLoaded) {
@@ -243,10 +238,10 @@ handleUserSelection(data)};
       <>
         <Box>
           <Container as="form" p={4} onSubmit={handleSubmit(onSubmit)}>
-            {/* <FormLabel>
+            <FormLabel>
               Select type of show or movie
-            </FormLabel> */}
-              <ControlledSelect
+            </FormLabel>
+               <Select 
                 isMulti         
                 name="types"
                 options={types}
@@ -254,9 +249,11 @@ handleUserSelection(data)};
                 placeholder="select one or more types"
                 closeMenuOnSelect={false}
                 control={control}
+                onChange={handleChangeType} // send value to hook form
+                
                 />
                           
-              <ControlledSelect
+              <Select
                 isMulti
                 defaultValues="sources"
                 name="sources"
@@ -265,10 +262,11 @@ handleUserSelection(data)};
                 placeholder="Select one or more viewing sources"
                 closeMenuOnSelect={false}
                 control={control}
+                onChange={handleChangeSource} // send value to hook form
               />
            
            
-              <ControlledSelect
+              <Select
                 isMulti
                 name="genres"
                 options={genres.map(({id, name}) => ({ value: id, label: name }))}
@@ -276,6 +274,7 @@ handleUserSelection(data)};
                 placeholder="Select one or more genres"
                 closeMenuOnSelect={false}
                 control={control}
+                onChange={handleChangeGenre} // send value to hook form
               />
               
             
@@ -283,44 +282,83 @@ handleUserSelection(data)};
                 {" "}
                 Find Your Shows/Movies{" "}
               </Button>
-          
-          </Container>
-        </Box>
+              </Container>
+          </Box>
+
+
+<Divider/>
 
         <Box>
-       <h2>
-          {searchedTitles.length
-            ? `Viewing ${searchedTitles.length} results:`
-            : ''}
-        </h2>
-        <Container>
-          {searchedTitles.map((detail) => {
-            return (
-              <Box key={detail.titleId} border='dark'>
-                {detail.poster ? (
-                  <Image src={detail.poster} alt={`The cover for ${detail.title}`} variant='top' />
-                ) : null}
+          <Heading> {displayedTitles.length
+              ? `Viewing ${displayedTitles.length} results:`
+              : ""}</Heading>
+          <Image  src={detail.poster}
+                      alt={`The cover for ${detail.title}`}
+                    />
+          
+          <Box display='flex' alignItems='baseline'>
+          <Badge borderRadius='full' px='2' colorScheme='blue'>
+            
+          </Badge>
+          <Box color='gray.500'
+            fontWeight='semibold'
+            letterSpacing='wide'
+            fontSize='xs'
+            textTransform='uppercase'
+            ml='2'>
+            {displayedTitles.map((detail) => {
+              return (
+                <Box id={detail.titleId}>
+                 </Box>)})}
+            </Box>
+            </Box>
 
-                
-                  <Heading>{detail.title}</Heading>
-                  <p className='small'>Runtime: {detail.runtimeMinutes}</p>
-                  <Text>{detail.plotOverview}</Text>
-                  {Auth.loggedIn() && (
-                    <Button
-                      disabled={savedTitleIds?.some((savedTitleId) => savedTitleId === detail.titleId)}
-                      className='btn-block btn-info'
-                      onClick={() => handleSaveTitle(detail.titleId)}>
-                      {savedTitleIds?.some((savedTitleId) => savedTitleId === detail.titleId)
-                        ? 'This book has already been saved!'
-                        : 'Save this Book!'}
-                    </Button>
-                  )}
-                
-              </Box>
-            );
-          })}
-        </Container>
-      </Box> 
+            
+          
+          
+            
+                  <Box>
+                    <Heading>{detail.title}</Heading>
+                    <p className="small">Runtime: {detail.runtimeMinutes}</p>
+                    <p className="small">Genres: {detail.genreNames}</p>
+                    {/* <p className='small'>Sources: {detail.sources.name}</p> */}
+                    <p className="small">Type: {detail.type}</p>
+                    <p className="small">User Rating: {detail.userRating}</p>
+                    <p className="small">
+                      Critics Rating: {detail.criticsRating}
+                    </p>
+
+                    <Text className="mt-3">
+                      {detail.plotOverview}
+                    </Text>
+                    {/* <p className="mt-3">
+                      <iframe
+                        width="230"
+                        height="205"
+                        src={detail.trailer}
+                        title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                        allowfullscreen
+                      ></iframe>
+                    </p> */}
+                    {Auth.loggedIn() && (
+                      <Button
+                        disabled={savedTitleIds?.some(
+                          (savedTitleId) => savedTitleId === detail.titleId
+                        )}
+                        className="btn-block btn-info mt-3"
+                        onClick={() => handleSaveTitle(detail.titleId)}
+                      >
+                        {savedTitleIds?.some(
+                          (savedTitleId) => savedTitleId === detail.titleId
+                        )
+                          ? "Added to your Watchlist"
+                          : "Add to Watchlist"}
+                      </Button>
+                    )}
+                  </Box>          
+                  </Box>
       </>
     );
   }
